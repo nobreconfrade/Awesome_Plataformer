d888481cd2138316cb529da66c706050acdebf7e
William Pereira <willpereiraBR@gmail.com>
2020-03-09 20:48:14 -0300

    Player moviment right and left
    Still a little clanky since player slides to side and need to lose speed faster when button stops to be pressed.

---
+++ Assets/Scripts/Player/PlayerMoviment.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+
+    public float speed = 5f;
+
+    private Rigidbody2D body;
+    private Animator animator;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        playerWalk();
+    }
+
+    void playerWalk()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        if(h == HorizontalConstants.right)
+        {
+            body.velocity = new Vector2(speed, body.velocity.y);
+        }
+        else if(h == HorizontalConstants.left)
+        {
+            body.velocity = new Vector2(-speed, body.velocity.y);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/Utils.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalConstants
+{
+    static public int right = 1;
+    static public int left = -1;
+}
