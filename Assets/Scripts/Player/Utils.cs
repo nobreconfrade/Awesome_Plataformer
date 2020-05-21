f09a65113b14ec3ebc21b8f6d30669193267bd43
William Pereira <willpereiraBR@gmail.com>
2020-05-20 22:32:37 -0300

    Utils better usage
    Change to utils, putting it in it own folder. Renaming file for a better understanding.

--- Assets/Scripts/Player/Utils.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HorizontalConstants
-{
-    static public int right = 1;
-    static public int left = -1;
-}
---
+++ Assets/Scripts/Utils/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalConstants
+{
+    static public int RIGHT = 1;
+    static public int LEFT = -1;
+}
+
+public class Tags
+{
+    static public string PLAYER = "Player";
+}
