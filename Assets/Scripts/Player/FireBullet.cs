de62248f59096bf5ef99b2c885ff282a1465f325
William Pereira <willpereiraBR@gmail.com>
2020-05-22 21:34:58 -0300

    Fireball mechanic
    Add fireball to the player.

---
+++ Assets/Scripts/Player/FireBullet.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBullet : MonoBehaviour
+{
+    private float speed = 10f;
+    private Animator animator;
+    private bool move_enable;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        move_enable = true;
+        StartCoroutine(DisableBullet());
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (move_enable)
+        {
+            Vector3 bullet_transform = transform.position;
+            bullet_transform.x += speed * Time.deltaTime;
+            transform.position = bullet_transform;
+        }
+    }
+
+    public float Speed
+    {
+        get
+        {
+            return speed;
+        }
+        set
+        {
+            speed = value;
+        }
+    }
+
+    IEnumerator DisableBullet(float timer = 5f)
+    {
+        yield return new WaitForSeconds(timer);
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == Tags.BEETLE || collision.gameObject.tag == Tags.SNAIL)
+        {
+            animator.Play("bullet_explode");
+            move_enable = false;
+            StartCoroutine(DisableBullet(0.3f));
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+
+    public GameObject fire_bullet;
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootBullet();
+    }
+    void ShootBullet()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            GameObject bullet = Instantiate(fire_bullet, transform.position, Quaternion.identity);
+            if(transform.localScale.x < 0)
+            {
+                bullet.GetComponent<FireBullet>().Speed *= -1;
+            }
+        }
+    }
+}
